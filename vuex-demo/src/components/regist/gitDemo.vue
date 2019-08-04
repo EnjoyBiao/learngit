72aa59bd3a9722b584da1f09d5294954d27f759a
chenbiao <154798220@qq.com>
2019-08-04 12:01:10 +0800

    12.00 合并master

--- vuex-demo/src/components/regist/gitDemo.vue
+++ vuex-demo/src/components/regist/gitDemo.vue
@@ -4,7 +4,7 @@
        <span>查看修改</span>
        <span>查看修改2 可视化工具来自（master）来自dev</span>
       <span>Creating a new bran dev master 解决冲突 分支上的改动</span>
-      <span>Creating a new bran dev master 新增dev</span>
+      <span>Creating a new bran dev master 新增dev122</span>
     </div>
 </template>
 
